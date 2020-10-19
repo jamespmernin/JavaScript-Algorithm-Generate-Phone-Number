d3f82e6b033599297da50da4bec8812f974a2ae1
James Mernin <jamespmernin@gmail.com>
2020-10-19 19:50:07 -0400

    Ternary applied

--- generatePhoneNumber.js
+++ generatePhoneNumber.js
@@ -4,10 +4,7 @@
 const generatePhoneNumber = nums => {
   let formattedNums = ('' + nums).replace(/\D/g, ''); // This takes care of the array condition.
   let regex = formattedNums.match(/^(\d{3})(\d{3})(\d{4})$/); // This splits the phone number into three segments of three, three, and four digits respectively
-  if (regex) {
-    return regex[1] + '-' + regex[2] + '-' + regex[3]; // This returns the corrected phone number with dashes where they are needed
-  }
-  return null; // In case for some reason the if statement fails
+  return regex ? regex[1] + '-' + regex[2] + '-' + regex[3] : null; // Returns the formatted number if the regex works and null if not
 }
 
 module.exports = generatePhoneNumber
